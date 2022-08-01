368f037e37058b49d31b4bd907b004e828edeb0a
strNophix <nvdpoel01@gmail.com>
2022-08-01 12:13:18 +0200

    Initial commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="px-2 lg:mx-auto  lg:max-w-[728px] relative min-h-screen">
+	<slot />
+	<footer class="text-xs absolute bottom-0 py-4 text-center w-full">
+		This site was created using Svelte and typed on Boba U4T's.
+	</footer>
+</div>
